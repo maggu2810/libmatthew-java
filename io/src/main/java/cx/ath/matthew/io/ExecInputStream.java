87e431165c3527d70e66ab7e0c921acc1092e5bb
Markus Rathgeb <maggu2810@gmail.com>
2017-03-26 11:50:44 +0200

    handle artifacts separately and prepare next dev. cycle
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- io/src/main/java/cx/ath/matthew/io/ExecInputStream.java
+++ io/src/main/java/cx/ath/matthew/io/ExecInputStream.java
@@ -2,17 +2,17 @@
  * Java Exec Pipe Library
  *
  * Copyright (c) Matthew Johnson 2005
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * 
+ *
  * To Contact the author, please email src@matthew.ath.cx
  *
  */
@@ -28,115 +28,149 @@
 package cx.ath.matthew.io;
 
 import java.io.FilterInputStream;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
  * Class to pipe an InputStream through a command using stdin/stdout.
  * E.g.
+ *
  * <pre>
- *    Reader r = new InputStreamReader(new ExecInputStream(new FileInputStream("file"), "command"));
+ * Reader r = new InputStreamReader(new ExecInputStream(new FileInputStream("file"), "command"));
  * </pre>
  */
-public class ExecInputStream extends FilterInputStream
-{
-   private Process proc;
-   private InputStream stdout;
-   private OutputStream stdin;
-   private InOutCopier copy;
+public class ExecInputStream extends FilterInputStream {
+    private final Process proc;
+    private final InputStream stdout;
+    private final OutputStream stdin;
+    private final InOutCopier copy;
 
-   /**
-    * Create a new ExecInputStream on the given InputStream
-    * using the process to filter the stream.
-    * @param is Reads from this InputStream
-    * @param p Filters data through stdin/out on this Process
-    */
-   public ExecInputStream(InputStream is, Process p) throws IOException
-   {
-      super(is);
-      proc = p;
-      stdin = p.getOutputStream();
-      stdout = p.getInputStream();
-      copy = new InOutCopier(in, stdin);
-      copy.start();
-   }
-   /**
-    * Create a new ExecInputStream on the given InputStream
-    * using the process to filter the stream.
-    * @param is Reads from this InputStream
-    * @param cmd Creates a Process from this string to filter data through stdin/out 
-    */
-   public ExecInputStream(InputStream is, String cmd) throws IOException
-   { this(is, Runtime.getRuntime().exec(cmd)); }
-   /**
-    * Create a new ExecInputStream on the given InputStream
-    * using the process to filter the stream.
-    * @param is Reads from this InputStream
-    * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out 
-    */
-   public ExecInputStream(InputStream is, String[] cmd) throws IOException
-   { this(is, Runtime.getRuntime().exec(cmd)); }
-   /**
-    * Create a new ExecInputStream on the given InputStream
-    * using the process to filter the stream.
-    * @param is Reads from this InputStream
-    * @param cmd Creates a Process from this string to filter data through stdin/out 
-    * @param env Setup the environment for the command
-    */
-   public ExecInputStream(InputStream is, String cmd, String[] env) throws IOException
-   { this(is, Runtime.getRuntime().exec(cmd, env)); }
-   /**
-    * Create a new ExecInputStream on the given InputStream
-    * using the process to filter the stream.
-    * @param is Reads from this InputStream
-    * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out 
-    * @param env Setup the environment for the command
-    */
-   public ExecInputStream(InputStream is, String[] cmd, String[] env) throws IOException
-   { this(is, Runtime.getRuntime().exec(cmd, env)); }
+    /**
+     * Create a new ExecInputStream on the given InputStream
+     * using the process to filter the stream.
+     *
+     * @param is Reads from this InputStream
+     * @param p Filters data through stdin/out on this Process
+     */
+    public ExecInputStream(final InputStream is, final Process p) throws IOException {
+        super(is);
+        proc = p;
+        stdin = p.getOutputStream();
+        stdout = p.getInputStream();
+        copy = new InOutCopier(in, stdin);
+        copy.start();
+    }
 
-   public void close() throws IOException
-   {
-      try {
-         proc.waitFor();
-      } catch (InterruptedException Ie)  {}
-      //copy.close();
-      try {
-         copy.join();
-      } catch (InterruptedException Ie)  {}
-      stdin.close();
-      in.close();
-      stdout.close();
-   }
-   public void flush() throws IOException
-   {
-      copy.flush();
-   }
-   public int	available() throws IOException
-   { return stdout.available(); } 
-   public int	read() throws IOException
-   { return stdout.read(); }
-   public int	read(byte[] b) throws IOException
-   { return stdout.read(b); }
-   public int	read(byte[] b, int off, int len) throws IOException
-   { return stdout.read(b, off, len); }
-   public long	skip(long n) throws IOException
-   { return stdout.skip(n); }
-   public void	mark(int readlimit)
-   {}
-   public boolean	markSupported()
-   { return false; }
-   public void	reset() 
-   {}
+    /**
+     * Create a new ExecInputStream on the given InputStream
+     * using the process to filter the stream.
+     *
+     * @param is Reads from this InputStream
+     * @param cmd Creates a Process from this string to filter data through stdin/out
+     */
+    public ExecInputStream(final InputStream is, final String cmd) throws IOException {
+        this(is, Runtime.getRuntime().exec(cmd));
+    }
 
-   public void finalize()
-   {
-      try {
-         close();
-      } catch (Exception e) {}
-   }
+    /**
+     * Create a new ExecInputStream on the given InputStream
+     * using the process to filter the stream.
+     *
+     * @param is Reads from this InputStream
+     * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out
+     */
+    public ExecInputStream(final InputStream is, final String[] cmd) throws IOException {
+        this(is, Runtime.getRuntime().exec(cmd));
+    }
+
+    /**
+     * Create a new ExecInputStream on the given InputStream
+     * using the process to filter the stream.
+     *
+     * @param is Reads from this InputStream
+     * @param cmd Creates a Process from this string to filter data through stdin/out
+     * @param env Setup the environment for the command
+     */
+    public ExecInputStream(final InputStream is, final String cmd, final String[] env) throws IOException {
+        this(is, Runtime.getRuntime().exec(cmd, env));
+    }
+
+    /**
+     * Create a new ExecInputStream on the given InputStream
+     * using the process to filter the stream.
+     *
+     * @param is Reads from this InputStream
+     * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out
+     * @param env Setup the environment for the command
+     */
+    public ExecInputStream(final InputStream is, final String[] cmd, final String[] env) throws IOException {
+        this(is, Runtime.getRuntime().exec(cmd, env));
+    }
+
+    @Override
+    public void close() throws IOException {
+        try {
+            proc.waitFor();
+        } catch (final InterruptedException Ie) {
+        }
+        // copy.close();
+        try {
+            copy.join();
+        } catch (final InterruptedException Ie) {
+        }
+        stdin.close();
+        in.close();
+        stdout.close();
+    }
+
+    public void flush() throws IOException {
+        copy.flush();
+    }
+
+    @Override
+    public int available() throws IOException {
+        return stdout.available();
+    }
+
+    @Override
+    public int read() throws IOException {
+        return stdout.read();
+    }
+
+    @Override
+    public int read(final byte[] b) throws IOException {
+        return stdout.read(b);
+    }
+
+    @Override
+    public int read(final byte[] b, final int off, final int len) throws IOException {
+        return stdout.read(b, off, len);
+    }
+
+    @Override
+    public long skip(final long n) throws IOException {
+        return stdout.skip(n);
+    }
+
+    @Override
+    public void mark(final int readlimit) {
+    }
+
+    @Override
+    public boolean markSupported() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void finalize() {
+        try {
+            close();
+        } catch (final Exception e) {
+        }
+    }
 }
-
-
-
--- io/src/main/java/cx/ath/matthew/io/ExecOutputStream.java
+++ io/src/main/java/cx/ath/matthew/io/ExecOutputStream.java
@@ -2,17 +2,17 @@
  * Java Exec Pipe Library
  *
  * Copyright (c) Matthew Johnson 2005
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * 
+ *
  * To Contact the author, please email src@matthew.ath.cx
  *
  */
@@ -28,110 +28,129 @@
 package cx.ath.matthew.io;
 
 import java.io.FilterOutputStream;
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
  * Class to pipe an OutputStream through a command using stdin/stdout.
  * E.g.
+ *
  * <pre>
- *    Writer w = new OutputStreamWriter(new ExecOutputStream(new FileOutputStream("file"), "command"));
+ * Writer w = new OutputStreamWriter(new ExecOutputStream(new FileOutputStream("file"), "command"));
  * </pre>
  */
-public class ExecOutputStream extends FilterOutputStream
-{
-   private Process proc;
-   private InputStream stdout;
-   private OutputStream stdin;
-   private InOutCopier copy;
+public class ExecOutputStream extends FilterOutputStream {
+    private final Process proc;
+    private final InputStream stdout;
+    private final OutputStream stdin;
+    private final InOutCopier copy;
 
-   /**
-    * Create a new ExecOutputStream on the given OutputStream
-    * using the process to filter the stream.
-    * @param os Writes to this OutputStream
-    * @param p Filters data through stdin/out on this Process
-    */
-   public ExecOutputStream(OutputStream os, Process p) throws IOException
-   {
-      super(os);
-      proc = p;
-      stdin = p.getOutputStream();
-      stdout = p.getInputStream();
-      copy = new InOutCopier(stdout, out);
-      copy.start();
-   }
-   /**
-    * Create a new ExecOutputStream on the given OutputStream
-    * using the process to filter the stream.
-    * @param os Writes to this OutputStream
-    * @param cmd Creates a Process from this string to filter data through stdin/out 
-    */
-   public ExecOutputStream(OutputStream os, String cmd) throws IOException
-   { this(os, Runtime.getRuntime().exec(cmd)); }
-   /**
-    * Create a new ExecOutputStream on the given OutputStream
-    * using the process to filter the stream.
-    * @param os Writes to this OutputStream
-    * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out 
-    */
-   public ExecOutputStream(OutputStream os, String[] cmd) throws IOException
-   { this(os, Runtime.getRuntime().exec(cmd)); }
-   /**
-    * Create a new ExecOutputStream on the given OutputStream
-    * using the process to filter the stream.
-    * @param os Writes to this OutputStream
-    * @param cmd Creates a Process from this string to filter data through stdin/out 
-    * @param env Setup the environment for the command
-    */
-   public ExecOutputStream(OutputStream os, String cmd, String[] env) throws IOException
-   { this(os, Runtime.getRuntime().exec(cmd, env)); }
-   /**
-    * Create a new ExecOutputStream on the given OutputStream
-    * using the process to filter the stream.
-    * @param os Writes to this OutputStream
-    * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out 
-    * @param env Setup the environment for the command
-    */
-   public ExecOutputStream(OutputStream os, String[] cmd, String[] env) throws IOException
-   { this(os, Runtime.getRuntime().exec(cmd, env)); }
+    /**
+     * Create a new ExecOutputStream on the given OutputStream
+     * using the process to filter the stream.
+     *
+     * @param os Writes to this OutputStream
+     * @param p Filters data through stdin/out on this Process
+     */
+    public ExecOutputStream(final OutputStream os, final Process p) throws IOException {
+        super(os);
+        proc = p;
+        stdin = p.getOutputStream();
+        stdout = p.getInputStream();
+        copy = new InOutCopier(stdout, out);
+        copy.start();
+    }
 
-   public void close() throws IOException
-   {
-      stdin.close();
-      try {
-         proc.waitFor();
-      } catch (InterruptedException Ie)  {}
-      //copy.close();
-      try {
-         copy.join();
-      } catch (InterruptedException Ie)  {}
-      stdout.close();
-      out.close();
-   }
-   public void flush() throws IOException
-   {
-      stdin.flush();
-      copy.flush();
-      out.flush();
-   }
-   public void write(byte[] b) throws IOException
-   {
-      stdin.write(b);
-   }
-   public void write(byte[] b, int off, int len) throws IOException
-   {
-      stdin.write(b, off, len);
-   }
-   public void write(int b) throws IOException
-   {
-      stdin.write(b);
-   }
-   public void finalize()
-   {
-      try {
-         close();
-      } catch (Exception e) {}
-   }
+    /**
+     * Create a new ExecOutputStream on the given OutputStream
+     * using the process to filter the stream.
+     *
+     * @param os Writes to this OutputStream
+     * @param cmd Creates a Process from this string to filter data through stdin/out
+     */
+    public ExecOutputStream(final OutputStream os, final String cmd) throws IOException {
+        this(os, Runtime.getRuntime().exec(cmd));
+    }
+
+    /**
+     * Create a new ExecOutputStream on the given OutputStream
+     * using the process to filter the stream.
+     *
+     * @param os Writes to this OutputStream
+     * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out
+     */
+    public ExecOutputStream(final OutputStream os, final String[] cmd) throws IOException {
+        this(os, Runtime.getRuntime().exec(cmd));
+    }
+
+    /**
+     * Create a new ExecOutputStream on the given OutputStream
+     * using the process to filter the stream.
+     *
+     * @param os Writes to this OutputStream
+     * @param cmd Creates a Process from this string to filter data through stdin/out
+     * @param env Setup the environment for the command
+     */
+    public ExecOutputStream(final OutputStream os, final String cmd, final String[] env) throws IOException {
+        this(os, Runtime.getRuntime().exec(cmd, env));
+    }
+
+    /**
+     * Create a new ExecOutputStream on the given OutputStream
+     * using the process to filter the stream.
+     *
+     * @param os Writes to this OutputStream
+     * @param cmd Creates a Process from this string array (command, arg, ...) to filter data through stdin/out
+     * @param env Setup the environment for the command
+     */
+    public ExecOutputStream(final OutputStream os, final String[] cmd, final String[] env) throws IOException {
+        this(os, Runtime.getRuntime().exec(cmd, env));
+    }
+
+    @Override
+    public void close() throws IOException {
+        stdin.close();
+        try {
+            proc.waitFor();
+        } catch (final InterruptedException Ie) {
+        }
+        // copy.close();
+        try {
+            copy.join();
+        } catch (final InterruptedException Ie) {
+        }
+        stdout.close();
+        out.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        stdin.flush();
+        copy.flush();
+        out.flush();
+    }
+
+    @Override
+    public void write(final byte[] b) throws IOException {
+        stdin.write(b);
+    }
+
+    @Override
+    public void write(final byte[] b, final int off, final int len) throws IOException {
+        stdin.write(b, off, len);
+    }
+
+    @Override
+    public void write(final int b) throws IOException {
+        stdin.write(b);
+    }
+
+    @Override
+    public void finalize() {
+        try {
+            close();
+        } catch (final Exception e) {
+        }
+    }
 }
-
